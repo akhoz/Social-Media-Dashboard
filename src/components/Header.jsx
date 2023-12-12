f99f2498057efd5e9b7e89173a5b66e09ca6e8c8
akhoz <adrian25092005@gmail.com>
2023-12-12 11:18:32 -0600

    Positioning Header elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,20 @@ import ThemeToggle from "./ThemeToggle.jsx";
 function Header(props) {
 
     return (
-        <div className="bg-headerDark grid grid-cols-1">
-            <p className="text-white">
-                Social Media Dashboard
-            </p>
-            <p className="text-titleDark">
-                Total Followers: {props.totalFollowers}
-            </p>
+        <div className="bg-headerDark grid grid-cols-1 py-5 px-4">
             <div>
                 <p className="text-white">
+                    Social Media Dashboard
+                </p>
+                <p className="text-titleDark">
+                    Total Followers: {props.totalFollowers}
+                </p>
+            </div>
+
+            <hr className="border-t-2 border-bgCardDark my-3" />
+
+            <div className="flex items-center">
+                <p className="text-white mr-auto">
                     Dark Mode
                 </p>
                 <ThemeToggle />
